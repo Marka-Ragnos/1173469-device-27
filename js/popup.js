21322b6db8e0e86ff3180052a59826b12aa67d09
Саня <dunkan-1@yandex.ru>
2020-03-10 09:43:57 +0300

    финал, подключил минимизированные стили и скрипты

--- js/popup.js
+++ js/popup.js
@@ -9,47 +9,47 @@ var storage = "";
 
 
 try {
-  storage = localStorage.getItem ("login");
+    storage = localStorage.getItem("login");
 } catch (err) {
-  isStorageSupport = false;
+    isStorageSupport = false;
 }
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-login-show");
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  } else {
-    login.focus();
-  }
+    evt.preventDefault();
+    popup.classList.add("modal-login-show");
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+        login.focus();
+    }
 });
 
 close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-login-show");
-  popup.classList.remove("modal-login-error");
+    evt.preventDefault();
+    popup.classList.remove("modal-login-show");
+    popup.classList.remove("modal-login-error");
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!login.value || !password.value) {
-    evt.preventDefault();
-    popup.classList.remove("modal-login-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-login-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", login.value);
+    if (!login.value || !password.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-login-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-login-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+        }
     }
-  }
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains("modal-login-show")) {
-      popup.classList.remove("modal-login-show");
-      popup.classList.remove("modal-login-error");
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-login-show")) {
+            popup.classList.remove("modal-login-show");
+            popup.classList.remove("modal-login-error");
+        }
     }
-  }
 })
